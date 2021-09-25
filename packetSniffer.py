480a77963463061475212e025597284157ab7209
sma98 <mdsmabeyrathne@gmail.com>
2021-09-25 12:40:34 +0530

    minor changes

--- packetSniffer.py
+++ packetSniffer.py
@@ -1,14 +1,24 @@
 import scapy.all as scapy
+from collections import Counter
+
+# Constants
+
+# network adapter to sniff
 NETWORK_ADAPTER = 'Wi-Fi'
+packet_counts = Counter()
 
-# test
 
+# sniff function that will return sniffed packet to process_sniffed_packet()
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    # @param iface = interface to sniff
+    # @param filter - filter what to sniff
+    scapy.sniff(iface=interface, filter="ip", store=False, prn=process_sniffed_packet, count=10)
 
 
 def process_sniffed_packet(packet):
-    print(packet)
+    print("From : ", packet[0][1].src, "  To : ", packet[0][1].dst)
+    # key = tuple(sorted([packet[0][1].src, packet[0][1].dst]))
+    # packet_counts.update([key])
 
 
 sniff(NETWORK_ADAPTER)
